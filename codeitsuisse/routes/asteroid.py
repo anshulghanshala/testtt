5b8216d02eb33e4c9e7dc205a51c5a13efbb4e4b
anshul6244 <anshul.ghanshala@kosmic.io>
2021-09-26 08:42:20 +0800

    Added

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -1,3 +1,108 @@
+#import logging
+#import json
+#
+#from flask import request, jsonify
+#from codeitsuisse import app
+#logger = logging.getLogger(__name__)
+#
+#
+#def longestPalindrome(s):
+#    dp = [[False] * len(s) for _ in range(len(s))]
+#    length: int = 0
+#    result: str = ''
+#    st = -1
+#    en = -1
+#    for end in range(len(s)):
+#        for start in range(end + 1):
+#            if start == end:
+#                dp[start][end] = True
+#            elif start + 1 == end:
+#                dp[start][end] = s[start] == s[end]
+#            else:
+#                dp[start][end] = s[start] == s[end] and dp[start + 1][end - 1]
+#
+#                if dp[start][end] and end - start + 1 > length:
+#                    length = end - start + 1
+#                    result = s[start:end + 1]
+#                    st = start
+#                    en = end
+#
+#    return st, en
+#
+#
+#def count_char(s,st,en):
+#    if st > 0:
+#        m = s[st]
+#        for i in range(st,0,-1):
+#            if s[i] == m:
+#                st = st - 1
+#            else:
+#                break
+#    if en < len(s):
+#        n = s[en]
+#        for i in range(en,len(s)):
+#            if s[i] == n:
+#                en = en + 1
+#            else:
+#                break
+#    return st,en
+#
+#
+#def total(list):
+#    total = 0
+#    for i in list:
+#        if i <= 6:
+#            total = total + (i*1)
+#        if i >= 7:
+#            total = total + (i*1.5)
+#        if i >= 10:
+#            total = total + (i*2)
+#    return total
+#
+#
+#def main_work(s):
+#    st, en = longestPalindrome(s)
+#
+#    if st == en == -1:
+#        return s,1,int(len(s)/2)
+#
+#    index = int((st+en)/2)
+#    st, en = count_char(s, st, en)
+#
+#    while(1):
+#        if (st) > 0 and en < len(s)-1:
+#            st, en = count_char(s, st, en)
+#        else:
+#            break
+#
+#    str = s[st:en]
+#
+#    res = {}
+#
+#    for keys in str:
+#        res[keys] = res.get(keys, 0) + 1
+#
+#    return s , total(res.values()) , index
+#
+#
+#@app.route('/asteroid', methods=['POST'])
+#def evaluateasteroid():
+#    data = request.get_json()
+#    logging.info("data sent for evaluation {}".format(data))
+#    inputValue = data.get("test_cases")
+#    res = []
+#    for s in inputValue:
+#        inp, score,origin = main_work(s)
+#        result = {
+#            "input": inp,
+#            "score": score,
+#            "origin": origin
+#        }
+#        res.append(result)
+#
+#    logging.info("My result :{}".format(res))
+#    return jsonify(res)
+
 import logging
 import json
 
@@ -6,99 +111,95 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 
-def longestPalindrome(s):
-    dp = [[False] * len(s) for _ in range(len(s))]
-    length: int = 0
-    result: str = ''
-    st = -1
-    en = -1
-    for end in range(len(s)):
-        for start in range(end + 1):
-            if start == end:
-                dp[start][end] = True
-            elif start + 1 == end:
-                dp[start][end] = s[start] == s[end]
-            else:
-                dp[start][end] = s[start] == s[end] and dp[start + 1][end - 1]
-
-                if dp[start][end] and end - start + 1 > length:
-                    length = end - start + 1
-                    result = s[start:end + 1]
-                    st = start
-                    en = end
-
-    return st, en
-
-
-def count_char(s,st,en):
-    if st > 0:
-        m = s[st]
-        for i in range(st,0,-1):
-            if s[i] == m:
-                st = st - 1
-            else:
-                break
-    if en < len(s):
-        n = s[en]
-        for i in range(en,len(s)):
-            if s[i] == n:
-                en = en + 1
-            else:
-                break
-    return st,en
-
-
-def total(list):
-    total = 0
-    for i in list:
-        if i <= 6:
-            total = total + (i*1)
-        if i >= 7:
-            total = total + (i*1.5)
-        if i >= 10:
-            total = total + (i*2)
-    return total
-
-
-def main_work(s):
-    st, en = longestPalindrome(s)
-    
-    if st == en == -1:
-        return s,1,int(len(s)/2)
-    
-    index = int((st+en)/2)
-    st, en = count_char(s, st, en)
-
-    while(1):
-        if (st) > 0 and en < len(s)-1:
-            st, en = count_char(s, st, en)
-        else:
-            break
-
-    str = s[st:en]
-
-    res = {}
-
-    for keys in str:
-        res[keys] = res.get(keys, 0) + 1
-        
-    return s , total(res.values()) , index
-
-
 @app.route('/asteroid', methods=['POST'])
 def evaluateasteroid():
+    def longestPalindrome(s):
+        dp = [[False] * len(s) for _ in range(len(s))]
+        length: int = 0
+        result: str = ''
+        st = -1
+        en = -1
+        for end in range(len(s)):
+            for start in range(end + 1):
+                if start == end:
+                    dp[start][end] = True
+                elif start + 1 == end:
+                    dp[start][end] = s[start] == s[end]
+                else:
+                    dp[start][end] = s[start] == s[end] and dp[start + 1][end - 1]
+
+                    if dp[start][end] and end - start + 1 > length:
+                        length = end - start + 1
+                        result = s[start:end + 1]
+                        st = start
+                    en = end
+
+        return st, en
+
+    def count_char(s, st, en):
+        if st > 0:
+            m = s[st]
+            for i in range(st, 0, -1):
+                if s[i] == m:
+                    st = st - 1
+                else:
+                    break
+        if en < len(s):
+            n = s[en]
+            for i in range(en, len(s)):
+                if s[i] == n:
+                    en = en + 1
+                else:
+                    break
+        return st, en
+
+    def total(list):
+        total = 0
+        for i in list:
+            if i <= 6:
+                total = total + (i*1)
+            if i >= 7:
+                total = total + (i*1.5)
+            if i >= 10:
+                total = total + (i*2)
+        return total
+
+    def main_work(s):
+        st, en = longestPalindrome(s)
+
+        if st == en == -1:
+            return s, 1, int(len(s)/2)
+
+        index = int((st+en)/2)
+        st, en = count_char(s, st, en)
+
+        while(1):
+            if (st) > 0 and en < len(s)-1:
+                st, en = count_char(s, st, en)
+            else:
+                break
+
+        str = s[st:en]
+
+        res = {}
+
+        for keys in str:
+            res[keys] = res.get(keys, 0) + 1
+
+        return s, total(res.values()), index
+
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("test_cases")
     res = []
     for s in inputValue:
-        inp, score,origin = main_work(s)
+        inp, score, origin = main_work(s)
         result = {
             "input": inp,
             "score": score,
             "origin": origin
         }
         res.append(result)
-    
+
     logging.info("My result :{}".format(res))
     return jsonify(res)
--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -1,83 +1,3 @@
-#import logging
-#import json
-#import random
-#
-#from flask import request, jsonify
-#
-#from codeitsuisse import app
-#
-#logger = logging.getLogger(__name__)
-#
-#
-#def makelist(list, slot, start,index):
-#    temp_list = []
-#    for i in range(start, slot):
-#        temp_list.append(list[index])
-#    return temp_list
-#
-#
-#
-#
-#def guessNumber(list,slots,data,result,total,start,index):
-#    if slots > max(total) > start:
-#        start = max(total)
-#        index = index + 1
-#        return makelist(list,slots,start,index)
-#
-#    if slots > max(total):
-#        if max(total) == start:
-#            index = index + 1
-#            return makelist(list, slots, start, index)
-#
-#
-#
-#
-#
-#@app.route('/decoder', methods=['POST'])
-#def evaluatedecoder():
-#    data = request.get_json()
-#    logging.info("data sent for evaluation {}".format(data))
-#    possibleValues = data.get("possible_values")
-#    slots = data.get("num_slots")
-#    history = data.get("history")
-#    history_size = len(history)
-#
-#    past_data_list = []
-#    past_data_result =[]
-#    total = []
-#    start = 0
-#    index = 0
-#
-#    if history_size == 0:
-#        result = {"answer":makelist(possibleValues, slots, 0, 0)}
-#        # res = []
-#        # res.append(result)
-#        logging.info("My results are :{}".format(result))
-#        return json.dumps(result)
-#
-#    else:
-#        for i in range(len(history)):
-#            past_data_list.append(i.get("output_received"))
-#            res = []
-#            result_data = i.get("result")
-#            if len(str(result_data)) > 1:
-#                res = [int(a) for a in str(result_data)]
-#                total.append(sum(res))
-#                past_data_result.append(res)
-#            else:
-#                res.append(0)
-#                res.append(result_data)
-#                total.append(result_data)
-#                past_data_result.append(res)
-#
-#
-#
-#    result = {"answer":guessNumber(possibleValues,slots,past_data_list,past_data_result,total,start,index)}
-#    logging.info("My result :{}".format(result))
-#    #res = []
-#    #res.append(result)
-#    return json.dumps(result)
-    
 import logging
 import json
 import random
@@ -139,7 +59,7 @@ def guess_number(lists, slots, data, result, total, start, index):
 
 
 @app.route('/decoder', methods=['POST'])
-def decoder():
+def evaluatedecoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     possibleValues = data.get("possible_values")
